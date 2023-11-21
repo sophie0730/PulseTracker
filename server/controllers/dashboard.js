6fdf0a846badb0f35756ca5cbacd8f46c872f13a
Sophie <sophie010017@gmail.com>
2023-11-22 00:53:10 +0800

    fix: server cannot render react static file

--- server/controllers/dashboard.js
+++ server/controllers/dashboard.js
@@ -5,8 +5,12 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// export const showDashboard = (req, res) => {
+//   res.sendFile('dashboard.html', { root: `${__dirname}/../views` });
+// };
+
 export const showDashboard = (req, res) => {
-  res.sendFile('dashboard.html', { root: `${__dirname}/../views` });
+  res.sendFile('index.html', { root: `${__dirname}/../dist` });
 };
 
 export default showDashboard;
