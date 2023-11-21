2b699ff1fea80d3c427d25f147e6c98ede66b1c2
Sophie <sophie010017@gmail.com>
2023-11-21 19:58:13 +0800

    feat: add dropdown list to select time range

---
+++ client/src/components/alert.jsx
@@ -0,0 +1,12 @@
+export function Target() {
+  return (
+    <main>
+      <div className="title">
+        <h1>Alerts</h1>
+      </div>
+      <div className="alerts"></div>
+    </main>
+  );
+}
+
+export default Target;
