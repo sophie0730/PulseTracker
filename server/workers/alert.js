186a32da7f02a2e7867bf23d611563d58dd35010
Sophie <sophie010017@gmail.com>
2023-11-23 21:00:40 +0800

    fix: influxdb utils setting

--- server/workers/alert.js
+++ server/workers/alert.js
@@ -1,12 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import * as yaml from 'js-yaml';
-import * as fs from 'fs';
 import { checkAlerts } from '../models/alert.js';
+import { alertFile } from '../utils/yml-util.js';
 
 const CHECK_TIME_RANGE = '10s';
 const TIMEOUT = 10000;
 
-const alertFile = yaml.load(fs.readFileSync('./../yml/test.yml', 'utf-8'));
 const alertStates = {};
 
 function sendAlerts() {
