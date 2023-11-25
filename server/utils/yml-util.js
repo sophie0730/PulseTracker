bb31a634b93670dc5089e4ced5c5b337e9c57cb7
Sophie <sophie010017@gmail.com>
2023-11-25 17:56:45 +0800

    fix: yml file setting

--- server/utils/yml-util.js
+++ server/utils/yml-util.js
@@ -12,3 +12,7 @@ export const configFile = yaml.load(fs.readFileSync(filePath), 'utf-8');
 const alertFilePath = path.join(__dirname, '..', 'yml', configFile.rule_files[0]);
 export const alertFile = yaml.load(fs.readFileSync(alertFilePath), 'utf-8');
 // 現在先設只能有一個config檔!!
+
+//  email
+export const emailReceivers = configFile.alerting.static_configs.receivers;
+// server url
