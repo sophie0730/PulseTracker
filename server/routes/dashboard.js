5547b12e89c90d62f44be83b2fa6de7bc8bb750a
Sophie <sophie010017@gmail.com>
2023-11-26 23:50:54 +0800

    fix: fix rebase conflict

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,13 +4,9 @@ import { showDashboard } from '../controllers/dashboard.js';
 
 const router = Router();
 
-<<<<<<< HEAD
 router.route('/index.html').get(showDashboard);
 router.route('/graph').get(showDashboard);
 router.route('/alert').get(showDashboard);
 router.route('/target').get(showDashboard);
-=======
-router.route('/dashboard.html').get(showDashboard);
->>>>>>> dc592ac9 (fix: update project structure for adding react frameworke)
 
 export default router;
