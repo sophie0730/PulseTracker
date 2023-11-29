6a6c6ade5755f034dfbc01252b29f4b35da94b06
Sophie <sophie010017@gmail.com>
2023-11-29 23:42:04 +0800

    feat: complete pack binary executable function

--- server/utils/email-util.js
+++ server/utils/email-util.js
@@ -1,38 +1,41 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import FormData from 'form-data';
+import Mailgun from 'mailgun.js';
 import { emailReceivers } from './yml-util.js';
 
 dotenv.config({ path: `${process.cwd()}/.env` });
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.mailgun.org',
-  port: '587',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_TOKEN,
-  },
-});
-
-const receiversArr = emailReceivers.map((receiver) => receiver.target);
-
 export async function sendEmail(groupName, rule) {
-  transporter.sendMail({
-    from: 'no-reply@pulsetracker.com',
-    to: receiversArr,
-    subject: `System Alert: ${groupName}`,
-    html: `
-    <h1>Warning </h1>
-    <p>Your system endpoint has been reached the below alerting rule.</p>
-    <p>${rule}</p>
-    <p>Please take appropriate actions to avoid any system performance issues or downtime.</p>
-    <br />
-    <br />
-    <p>This is an automated message, please do not reply.</p>
-    <p>Your IT Support Team</p>
+  try {
+    const receiversArr = emailReceivers.map((receiver) => receiver.target);
+    const apiKey = process.env.EMAIL_API_KEY;
+    const domain = process.env.EMAIL_DOMAIN;
 
-    `,
-  }).then((info) => console.log({ info }))
-    .catch((error) => console.error(error));
+    const mailgun = new Mailgun(FormData).client({
+      username: 'api',
+      key: apiKey,
+    });
+
+    const data = {
+      from: 'no-reply@pulsetracker.com',
+      to: receiversArr,
+      subject: `System Alert: ${groupName}`,
+      html: `
+      <h1>Warning </h1>
+      <p>Your system endpoint has been reached the below alerting rule.</p>
+      <p>${rule}</p>
+      <p>Please take appropriate actions to avoid any system performance issues or downtime.</p>
+      <br />
+      <br />
+      <p>This is an automated message, please do not reply.</p>
+      <p>Your IT Support Team</p>
+      `,
+    };
+    await mailgun.messages.create(domain, data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default sendEmail;
