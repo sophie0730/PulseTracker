53786f77089f968f0f5088b8dc47d65a94bd8ca8
Sophie <sophie010017@gmail.com>
2023-12-04 16:04:09 +0800

    delete: delete unnecessary controller

--- server/controllers/heartbeat.js
+++
@@ -1,9 +0,0 @@
-import * as heartbeatManager from '../models/heartbeat.js';
-
-export function receiveHeartbeat(req, res) {
-  const { workerId } = req.query;
-  heartbeatManager.recordHeartbeat(workerId);
-  res.send('Heartbeat received');
-}
-
-export default receiveHeartbeat;
